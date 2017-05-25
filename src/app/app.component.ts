df216c93fdde41e7bda62ffa571f03ff9deee4bf
Guangyuan Cai <Cai.gyuan@gmail.com>
2017-05-25 16:21:26 -0500

    edited some side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
 
 import {LoginPage} from "../pages/login-page/login-page";
 
 import { ClassListPage } from '../pages/class-list/class-list';
+
+
 
 
 @Component({
@@ -26,8 +27,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
+      { title: 'Login', component: LoginPage },
+      //{ title: 'List', component: ListPage },
       { title: 'Classes', component: ClassListPage },
     ];
 
